6c99a0664e85545bebe5060fc35cdfd0af147ed9
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-02-29 23:12:55 -0300

    Funcionalidade de adicionar o estilo desejado (bold, itálico, ...) parcialmente adicionada

--- main.js
+++ main.js
@@ -1,28 +1,48 @@
+//Variables
+
+let selectedText = '';
+
 const boldEl = document.getElementById('bold');
 const italicEl = document.getElementById('italic');
 const strikethroughEl = document.getElementById('strikethrough');
 const underlinedEl = document.getElementById('underlined');
 const linkEl = document.getElementById('link');
 
-const textAreaEl = document.querySelector('.inputText textarea');
+const divEl = document.querySelector('.inputText div');
+const pEl = document.querySelector('.inputText div p');
 
-var selectedText = '';
+//Function that catch the selected text and add the respectively tag
+function addTagAround(tagHTML) {
+     /* anchor = o começo da seleção do usuario
+        focus = termino da seleção
+        ANCHOR CAN BE PLACED BEFORE THE FOCUS OR VICE VERSA, DEPENDING ON THE DIRECTION YOU MADE YOUR SELECTION.
+    */
+   selectedText = document.getSelection().toString();
 
-// setInterval(getSelectedText, 1000);
+   const range = document.createRange();
+   const tag = document.createElement(tagHTML);
+   
+   range.selectNode(divEl);
+   range.surroundContents(tag);
 
-function getSelectedText() {
-    if (window.getSelection) {
-        return textAreaEl.value.substring(textAreaEl.selectionStart, textAreaEl.selectionEnd)
-    }
+   console.log(`range: ${range}`);
+   console.log(`Bold tag: ${tag}`);
+   
+//    var ranger  = selectedText.getRangeAt(0);
+
+//    console.log(selectedText.anchorNode); 
+//    console.log("Começa: " + selectedText.anchorOffset); 
+//    console.log("Termina: " + selectedText.focusOffset); 
+//    console.log(`range:  ${ranger}`); 
 }
 
-boldEl.addEventListener('click', () => {
-    
-   
+
+boldEl.addEventListener('click', function() {
+    addTagAround('b');
 });
 
 italicEl.onclick = () => {
-
+    addTagAround('i');
 }
 
 strikethroughEl.onclick = () => {
