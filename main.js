653cb280a4ed1b9c78475198d5341eef9b84a735
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-02-29 23:43:06 -0300

    Estilo de Link melhorado, adicionando a url e definindo para abrir em outra página

--- main.js
+++ main.js
@@ -12,48 +12,63 @@ const divEl = document.querySelector('.inputText div');
 const pEl = document.querySelector('.inputText div p');
 
 //Function that catch the selected text and add the respectively tag
-function addTagAround(tagHTML) {
-     /* anchor = o começo da seleção do usuario
-        focus = termino da seleção
-        ANCHOR CAN BE PLACED BEFORE THE FOCUS OR VICE VERSA, DEPENDING ON THE DIRECTION YOU MADE YOUR SELECTION.
-    */
-   selectedText = document.getSelection().toString();
+function addStyleTag(tagHTML) {
 
-   const range = document.createRange();
-   const tag = document.createElement(tagHTML);
-   
-   range.selectNode(divEl);
-   range.surroundContents(tag);
+    /* anchor = o começo da seleção do usuario
+       focus = termino da seleção
+       ANCHOR CAN BE PLACED BEFORE THE FOCUS OR VICE VERSA, DEPENDING ON THE DIRECTION YOU MADE YOUR SELECTION.
+   */
 
-   console.log(`range: ${range}`);
-   console.log(`Bold tag: ${tag}`);
-   
-//    var ranger  = selectedText.getRangeAt(0);
+    selectedText = document.getSelection();
 
-//    console.log(selectedText.anchorNode); 
-//    console.log("Começa: " + selectedText.anchorOffset); 
-//    console.log("Termina: " + selectedText.focusOffset); 
-//    console.log(`range:  ${ranger}`); 
+    const range = document.createRange();
+    const tag = document.createElement(tagHTML);
+
+    if (tagHTML === 'a') {
+        tag.setAttribute('href', 'https://github.com/pferreirafabricio');
+        tag.setAttribute('target', '_blank');
+    }
+
+    range.selectNode(pEl);
+
+    //    range.setStart(pEl, selectedText.anchorOffset);
+    //    range.setEnd(pEl, selectedText.anchorOffset);
+
+    console.log(`range.selectNode: ${range.selectNode(pEl)}`);
+    range.surroundContents(tag);
+
+    console.log(`range: ${range}`);
+    console.log(`Bold tag: ${tag}`);
+
+    //    var ranger  = selectedText.getRangeAt(0);
+
+    //    console.log(selectedText.anchorNode); 
+    //    console.log("Começa: " + selectedText.anchorOffset); 
+    //    console.log("Termina: " + selectedText.anchorOffset); 
+    //    console.log(`range:  ${ranger}`); 
 }
 
 
-boldEl.addEventListener('click', function() {
-    addTagAround('b');
+boldEl.addEventListener('click', function () {
+    addStyleTag('b');
 });
 
 italicEl.onclick = () => {
-    addTagAround('i');
+    addStyleTag('i');
 }
 
 strikethroughEl.onclick = () => {
-
+    addStyleTag('s');
 }
 
 underlinedEl.onclick = () => {
-
+    addStyleTag('u');
 }
 
 linkEl.onclick = () => {
-
+    // const aEl = document.createElement('a').textContent;
+    // aEl.setAttribute('href', 'https://github.com/pferreirafabricio');
+    // console.log(aEl.toString());
+    addStyleTag('a');
 }
 
