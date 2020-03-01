8bc27e0bdda13a209dcc3e73be789d8def6020fa
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-03-01 13:11:57 -0300

    Screenshot do projeto adicionada, limpeza do código JS

--- main.js
+++ main.js
@@ -10,47 +10,48 @@ const linkEl = document.getElementById('link');
 
 const divEl = document.querySelector('.inputText div');
 
-//Function that catch the selected text and add the respectively tag
-
 function addStyleTag(tagHTML) {
 
-    //Getting the DOM element and the text
     selectedNode = document.getSelection();
-    let selectionText = selectedNode.toString();
-
-    //Creating the respectively tag 
-    let tag = document.createElement(tagHTML);
-    
-    tag.textContent = selectionText;
-
-    if (tagHTML === 'a') {
-        tag.setAttribute('href', 'https://github.com/pferreirafabricio');
-        tag.setAttribute('target', '_blank');
-    }
-
     let range = selectedNode.getRangeAt(0);
-    range.deleteContents();
-    range.insertNode(tag);
+   
+    let parentNode = range.endContainer.parentNode.tagName;
+
+    if (parentNode === 'P') {
+        prompt('Só podemos editar textos dentro do espaço delimitado, ok?', 'Clique em OK se não um filhote de leão marinho irá morrer...');
+    }
+    else {
+        let tag = document.createElement(tagHTML);
+        let selectionText = document.createTextNode(selectedNode.toString());
+        tag.appendChild(selectionText);
+
+        if (tagHTML === 'a') {
+            tag.setAttribute('href', 'https://github.com/pferreirafabricio');
+            tag.setAttribute('target', '_blank');
+        }
+
+        range.deleteContents();
+        range.insertNode(tag);
+    }
 }
 
-
-boldEl.addEventListener('click', function () {
+boldEl.onclick = () => {
     addStyleTag('b');
-});
+};
 
 italicEl.onclick = () => {
     addStyleTag('i');
-}
+};
 
 strikethroughEl.onclick = () => {
     addStyleTag('s');
-}
+};
 
 underlinedEl.onclick = () => {
     addStyleTag('u');
-}
+};
 
 linkEl.onclick = () => {
     addStyleTag('a');
-}
+};
 
